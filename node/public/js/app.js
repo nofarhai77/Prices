9573a97217f9df9c5e2c2f5e2d9ec4ce18d8f50d
nofar <nofarhaim@mail.tau.ac.il>
2023-10-22 19:43:18 +0300

    updating get_item() function

--- node/public/js/app.js
+++ node/public/js/app.js
@@ -10,7 +10,7 @@ function get_subchains(chain) {
     return fetch("chain/" + chain + "/subchains")
 }
 
-/* find if a specific product had a discount in the previous day */
+/* find the price of a specific product */
 function get_item(itemName) {
     return fetch("item/" + itemName)
 }
