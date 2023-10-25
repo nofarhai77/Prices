5fdbb321b91178b22e47e5e4d7966bab79c54bd6
nofar <nofarhai77@gmail.com>
2023-10-25 20:44:54 +0300

    add query for autocomplete in 'item' field

--- node/server.js
+++ node/server.js
@@ -37,8 +37,7 @@ app.get('/chains', (req, res) => {
 
 app.get('/chain/:chainId', (req, res) => {
   const chainId = req.params['chainId'];
-  const sql = "SELECT id, name FROM prices.chains " +
-              `WHERE id=${chainId};`
+  const sql = "SELECT id, name FROM prices.chains " + `WHERE id=${chainId};`
   console.log(sql)
   db.query(sql, function (err, result) {
     console.log("Result: " + JSON.stringify(result));
@@ -49,8 +48,7 @@ app.get('/chain/:chainId', (req, res) => {
 
 app.get('/chain/:chainId/subchains', (req, res) => {
   const chainId = req.params['chainId'];
-  const sql = "SELECT id, name FROM prices.subchains " +
-              `WHERE chain=${chainId};`
+  const sql = "SELECT id, name FROM prices.subchains " + `WHERE chain=${chainId};`
   console.log(sql)
   db.query(sql, function (err, result) {
     console.log("Result: " + JSON.stringify(result));
@@ -63,7 +61,7 @@ app.get('/chain/:chainId/subchain/:subchainId', (req, res) => {
   const chainId = req.params['chainId'];
   const subchainId = req.params['subchainId'];
   const sql = "SELECT id, `chain`, name FROM prices.subchains " +
-      `WHERE chain=${chainId} AND id=${subchainId};`
+              `WHERE chain=${chainId} AND id=${subchainId};`
   console.log(sql)
   db.query(sql, function (err, result) {
     console.log("Result: " + JSON.stringify(result));
@@ -77,7 +75,7 @@ app.get('/chain/:chainId/subchain/:subchainId/store/:storeId', (req, res) => {
   const subchainId = req.params['subchainId'];
   const storeId = req.params['storeId'];
   const sql = "SELECT id, `chain`, subchain, `type`, name, city, address, zipcode FROM prices.Stores " +
-      `WHERE chain=${chainId} AND subchain=${subchainId} AND id=${storeId};`
+              `WHERE chain=${chainId} AND subchain=${subchainId} AND id=${storeId};`
   console.log(sql)
   db.query(sql, function (err, result) {
     console.log("Result: " + JSON.stringify(result));
@@ -86,20 +84,44 @@ app.get('/chain/:chainId/subchain/:subchainId/store/:storeId', (req, res) => {
   });
 })
 
-/* find the price of a specific product */
-app.get('/item/:itemName', (req, res) => {
-  const itemName = req.params['itemName'];
-  const sql = "SELECT name, id, unit_qty FROM prices.items " +
-      `WHERE name='${itemName}';`
+// /* OLD - find the price of specific product */
+// app.get('/item/:itemName', (req, res) => {
+//   const itemName = req.params['itemName'];
+//   const sql = "SELECT name, id, unit_qty FROM prices.items " + `WHERE name='${itemName}';`
+//   console.log(sql)
+//   db.query(sql, function (err, result) {
+//     console.log("Result: " + JSON.stringify(result));
+//     if (err) throw err;
+//     res.send(JSON.stringify(result));
+//   });
+// })
+
+
+// /* find the price of specific product */
+// app.get('/item/:itemId', (req, res) => {
+//   const itemId = req.params['itemId'];
+//   const sql = "SELECT name, id, unit_qty FROM prices.items " + `WHERE id='${itemId}';`
+//   console.log(sql)
+//   db.query(sql, function (err, result) {
+//     console.log("Result: " + JSON.stringify(result));
+//     if (err) throw err;
+//     res.send(JSON.stringify(result));
+//   });
+// })
+
+/* using for autocomplete in 'item' field */
+app.get('/item', (req, res) => {
+  const itemStartWith = req.query['startWith'];
+  const sql = `select id, name from prices.items where name like '${itemStartWith}%' limit 10`;
   console.log(sql)
   db.query(sql, function (err, result) {
     console.log("Result: " + JSON.stringify(result));
     if (err) throw err;
-    res.send(JSON.stringify(result));
+    res.send(JSON.stringify(result));  
   });
 })
 
-/* using for 'city' autocomplete field */
+/* using for autocomplete in 'city' field */
 app.get('/cities', (req, res) => {
   const startWith = req.query['startWith'];
   const sql = `select distinct city from prices.stores where city like '${startWith}%'`;
@@ -114,8 +136,7 @@ app.get('/cities', (req, res) => {
 /* get all stores in a specific city */
 app.get('/stores', (req, res) => {
   const city = req.query['city'];
-  const sql = "SELECT name, address FROM prices.Stores " +
-      `WHERE city='${city}';`
+  const sql = "SELECT name, address FROM prices.Stores " + `WHERE city='${city}';`
   console.log(sql)
   db.query(sql, function (err, result) {
     console.log("Result: " + JSON.stringify(result));
