706841a3a1d494723c0aa154f3a135c3edbe9be5
nofar <nofarhai77@gmail.com>
2023-10-26 19:08:04 +0300

    add get_item_prices() function

--- node/public/js/app.js
+++ node/public/js/app.js
@@ -1,6 +1,3 @@
-function get_store(chain, subchain, store_id) {
-    return fetch("chain/" + chain + "/subchain/" + subchain + "/store/" + store_id)
-}
 
 function get_chains() {
     return fetch("chains")
@@ -10,14 +7,8 @@ function get_subchains(chain) {
     return fetch("chain/" + chain + "/subchains")
 }
 
-// /* find the price of a specific product */
-// function get_item(itemName) {
-//     return fetch("item/" + itemName)
-// }
-
-/* for autocomplete function - return items with given prefix */
-function get_item_by_prefix(prefix) {
-    return fetch("item?startWith=" + prefix)
+function get_store(chain, subchain, store_id) {
+    return fetch("chain/" + chain + "/subchain/" + subchain + "/store/" + store_id)
 }
 
 /* for autocomplete function - return cities with given prefix */
@@ -28,4 +19,14 @@ function get_cities_by_prefix(prefix) {
 /* show all stores in a specific city */
 function get_stores_in_city(city) {
     return fetch("stores?city=" + city)
+}
+
+/* for autocomplete function - return items with given prefix */
+function get_item_by_prefix(prefix) {
+    return fetch("item?itemStartWith=" + prefix)
+}
+
+/* find the price of a specific product */
+function get_item_prices(itemId) {
+    return fetch("item/" + itemId)
 }
