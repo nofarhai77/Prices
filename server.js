37f0b0a6fa7c2222f5bc0cfb387162e17211e73b
nofar <nofarhai77@gmail.com>
2024-08-03 14:16:03 +0300

    Dynamic port.

--- server.js
+++ server.js
@@ -161,5 +161,5 @@ app.get('/item/:itemId', (req, res) => {
 // starts a port and host
 // making the localhost for the connections to listen to incoming requests from a client
 app.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${process.env.PORT || port}/`);
 });
